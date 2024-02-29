cf067cd51d67f938286c2f5b5c653f64c4b0a0af
Cristian Ghezzi <cristian@ghezzi.net>
2024-02-29 18:18:59 +0100

    Server launch

---
+++ springmvc/MyDoodle/src/main/webapp/res/js/wcp.js
@@ -0,0 +1,12 @@
+// wcp.js
+
+(function( wcp ) {
+	"use strict";
+	
+	// Namespace trick explained here: http://stackoverflow.com/a/5947280/587641
+	// For a public property or function, use "wcp.xxx = ..."
+	// For a private property use "var xxx = "
+	// For a private function use "function xxx(..."
+
+	
+}( window.wcp = window.wcp || {} ));	
